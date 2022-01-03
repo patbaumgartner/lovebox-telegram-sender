5255816c3bc317c184ac3973229d753f2c9b64f0
Patrick Baumgartner <patrick.baumgartner@42talents.com>
2022-01-03 07:48:13 +0100

    Using Noto Emoji Font. Closes #1

--- src/main/java/com/patbaumgartner/lovebox/telegram/sender/LoveboxTelegramSenderApplication.java
+++ src/main/java/com/patbaumgartner/lovebox/telegram/sender/LoveboxTelegramSenderApplication.java
@@ -2,11 +2,18 @@ package com.patbaumgartner.lovebox.telegram.sender;
 
 import com.patbaumgartner.lovebox.telegram.sender.rest.clients.LoveboxRestClientProperties;
 import com.patbaumgartner.lovebox.telegram.sender.telegram.LoveboxBotProperties;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @EnableFeignClients
 @EnableScheduling
@@ -16,6 +23,32 @@ public class LoveboxTelegramSenderApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LoveboxTelegramSenderApplication.class, args);
+    }
+
+    @Bean
+    @Profile("font-debug")
+    public CommandLineRunner commandLineRunner() {
+        return (args) -> {
+            String[] fonts = GraphicsEnvironment.getLocalGraphicsEnvironment()
+                    .getAvailableFontFamilyNames();
+            System.out.println(fonts.length + " font families installed.");
+
+            List<String> supportedFonts = new ArrayList<>();
+            for (String fontName : fonts) {
+                Font f = new Font(fontName, Font.PLAIN, 1);
+                System.out.println(fontName);
+                if (f.canDisplayUpTo("🚀️ you") < 0) {
+                    supportedFonts.add(fontName);
+                }
+            }
+
+            System.out.println("***");
+
+            System.out.println(supportedFonts.size() + " font families support emojis.");
+            for (String fontName : supportedFonts) {
+                System.out.println(fontName);
+            }
+        };
     }
 
 }
