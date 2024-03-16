5782ca9d70272f2aa91638e6bfad3540aaa648b7
Patrick Baumgartner <patrick.baumgartner@42talents.com>
2024-03-16 20:23:29 +0100

    Updating to Http Interface clients

--- src/main/java/com/patbaumgartner/lovebox/telegram/sender/rest/clients/LoginWithPasswordResponseBody.java
+++ src/main/java/com/patbaumgartner/lovebox/telegram/sender/rest/clients/LoginWithPasswordResponseBody.java
@@ -2,7 +2,7 @@ package com.patbaumgartner.lovebox.telegram.sender.rest.clients;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public record LoginWithPasswordResponseBody(@JsonProperty("_id") String id, String firstName,
-                                            String email, String token) {
+public record LoginWithPasswordResponseBody(@JsonProperty("_id") String id, String firstName, String email,
+		String token) {
 
 }
--- src/main/java/com/patbaumgartner/lovebox/telegram/sender/services/ImageService.java
+++ src/main/java/com/patbaumgartner/lovebox/telegram/sender/services/ImageService.java
@@ -25,132 +25,133 @@ import java.util.Random;
 @RequiredArgsConstructor
 public class ImageService {
 
-    public static final int DISPLAY_WIDTH = 1280;
-    public static final int DISPLAY_HEIGHT = 960;
-    public static final int BORDER_WIDTH = 20;
-    public static final int INITIAL_FONT_SIZE = 18;
-    // Emoji Font Limitations: https://mail.openjdk.java.net/pipermail/2d-dev/2021-May/012975.html
-    public static final int MAX_EMOJI_FONT_SIZE = 100;
-    public static final String FONT_NAME = "Sans";
+	public static final int DISPLAY_WIDTH = 1280;
 
-    public final ResourceLoader resourceLoader;
+	public static final int DISPLAY_HEIGHT = 960;
 
-    @SneakyThrows
-    public Pair<String, byte[]> resizeImageToPair(File file, String text) {
-        BufferedImage originalImage = ImageIO.read(file);
-        BufferedImage resizedImage =
-                Scalr.resize(
-                        originalImage,
-                        Scalr.Method.AUTOMATIC,
-                        Scalr.Mode.AUTOMATIC,
-                        DISPLAY_WIDTH,
-                        DISPLAY_HEIGHT,
-                        Scalr.OP_ANTIALIAS);
+	public static final int BORDER_WIDTH = 20;
 
-        BufferedImage image = new BufferedImage(DISPLAY_WIDTH, DISPLAY_HEIGHT, BufferedImage.TYPE_INT_ARGB);
-        Graphics2D graphics = image.createGraphics();
-        graphics.setColor(Color.black);
-        graphics.fillRect(0, 0, DISPLAY_WIDTH, DISPLAY_HEIGHT);
+	public static final int INITIAL_FONT_SIZE = 18;
 
-        int x = (DISPLAY_WIDTH - resizedImage.getWidth()) / 2;
-        int y = (DISPLAY_HEIGHT - resizedImage.getHeight()) / 2;
+	// Emoji Font Limitations:
+	// https://mail.openjdk.java.net/pipermail/2d-dev/2021-May/012975.html
+	public static final int MAX_EMOJI_FONT_SIZE = 100;
 
-        graphics.drawImage(resizedImage, x, y, null);
+	public static final String FONT_NAME = "Sans";
 
-        if (text != null) {
-            drawCenteredMessage(graphics, text);
-        }
+	public final ResourceLoader resourceLoader;
 
-        graphics.dispose();
+	@SneakyThrows
+	public Pair<String, byte[]> resizeImageToPair(File file, String text) {
+		BufferedImage originalImage = ImageIO.read(file);
+		BufferedImage resizedImage = Scalr.resize(originalImage, Scalr.Method.AUTOMATIC, Scalr.Mode.AUTOMATIC,
+				DISPLAY_WIDTH, DISPLAY_HEIGHT, Scalr.OP_ANTIALIAS);
 
-        return constructImagePair(image);
-    }
+		BufferedImage image = new BufferedImage(DISPLAY_WIDTH, DISPLAY_HEIGHT, BufferedImage.TYPE_INT_ARGB);
+		Graphics2D graphics = image.createGraphics();
+		graphics.setColor(Color.black);
+		graphics.fillRect(0, 0, DISPLAY_WIDTH, DISPLAY_HEIGHT);
 
-    @SneakyThrows
-    public Pair<String, byte[]> createTextImageToPair(String message) {
-        BufferedImage image = new BufferedImage(DISPLAY_WIDTH, DISPLAY_HEIGHT, BufferedImage.TYPE_INT_ARGB);
-        Graphics2D graphics = image.createGraphics();
+		int x = (DISPLAY_WIDTH - resizedImage.getWidth()) / 2;
+		int y = (DISPLAY_HEIGHT - resizedImage.getHeight()) / 2;
 
-        // Set background color
-        Random rnd = new Random();
-        int red = rnd.nextInt(256);
-        int green = rnd.nextInt(256);
-        int blue = rnd.nextInt(256);
-        Color color = new Color(red, green, blue);
-        graphics.setColor(color);
-        graphics.fillRect(0, 0, DISPLAY_WIDTH, DISPLAY_HEIGHT);
+		graphics.drawImage(resizedImage, x, y, null);
 
-        if (message != null) {
-            drawCenteredMessage(graphics, message);
-        }
+		if (text != null) {
+			drawCenteredMessage(graphics, text);
+		}
 
-        graphics.dispose();
+		graphics.dispose();
 
-        return constructImagePair(image);
-    }
+		return constructImagePair(image);
+	}
 
-    @SneakyThrows
-    public Pair<String, byte[]> createFixedImageToPair() {
-        Resource resource = resourceLoader.getResource("lovebox.jpeg");
-        Image image = ImageIO.read(resource.getInputStream());
-        image = image.getScaledInstance(DISPLAY_WIDTH, DISPLAY_HEIGHT, Image.SCALE_SMOOTH);
-        BufferedImage bufferedImage = new BufferedImage(DISPLAY_WIDTH, DISPLAY_HEIGHT, BufferedImage.TYPE_INT_ARGB);
+	@SneakyThrows
+	public Pair<String, byte[]> createTextImageToPair(String message) {
+		BufferedImage image = new BufferedImage(DISPLAY_WIDTH, DISPLAY_HEIGHT, BufferedImage.TYPE_INT_ARGB);
+		Graphics2D graphics = image.createGraphics();
 
-        Graphics2D graphics = bufferedImage.createGraphics();
-        graphics.drawImage(image, 0, 0, null);
+		// Set background color
+		Random rnd = new Random();
+		int red = rnd.nextInt(256);
+		int green = rnd.nextInt(256);
+		int blue = rnd.nextInt(256);
+		Color color = new Color(red, green, blue);
+		graphics.setColor(color);
+		graphics.fillRect(0, 0, DISPLAY_WIDTH, DISPLAY_HEIGHT);
 
-        graphics.dispose();
+		if (message != null) {
+			drawCenteredMessage(graphics, message);
+		}
 
-        return constructImagePair(bufferedImage);
-    }
+		graphics.dispose();
 
-    protected void drawCenteredMessage(Graphics2D graphics, String text) {
-        String message = text.strip();
+		return constructImagePair(image);
+	}
 
-        FontMetrics initialFm = graphics.getFontMetrics();
-        log.debug("Using initial font with settings: {}", initialFm);
+	@SneakyThrows
+	public Pair<String, byte[]> createFixedImageToPair() {
+		Resource resource = resourceLoader.getResource("lovebox.jpeg");
+		Image image = ImageIO.read(resource.getInputStream());
+		image = image.getScaledInstance(DISPLAY_WIDTH, DISPLAY_HEIGHT, Image.SCALE_SMOOTH);
+		BufferedImage bufferedImage = new BufferedImage(DISPLAY_WIDTH, DISPLAY_HEIGHT, BufferedImage.TYPE_INT_ARGB);
 
-        // Calculate max font
-        graphics.setColor(Color.white);
-        Font newFont = new Font(FONT_NAME, Font.PLAIN, INITIAL_FONT_SIZE);
-        graphics.setFont(newFont);
-        FontMetrics newFm = graphics.getFontMetrics();
-        String[] lines = message.split("\n");
-        // int stringWidth = initialFm.stringWidth(text) + 2 * BORDER_WIDTH;
-        int stringWidth = Arrays.stream(lines)
-                .map(line -> newFm.stringWidth(line) + 2 * BORDER_WIDTH)
-                .mapToInt(v -> v)
-                .max()
-                .orElseThrow(NoSuchElementException::new);
-        double widthRatio = (double) DISPLAY_WIDTH / (double) stringWidth;
+		Graphics2D graphics = bufferedImage.createGraphics();
+		graphics.drawImage(image, 0, 0, null);
 
-        int newFontSize = (int) (newFont.getSize() * widthRatio);
-        int fontSizeToUse = Math.min(newFontSize, MAX_EMOJI_FONT_SIZE);
-        Font finalFont = new Font(newFont.getName(), newFont.getStyle(), fontSizeToUse);
-        graphics.setFont(finalFont);
+		graphics.dispose();
 
-        // Draw centered string
-        FontMetrics fm = graphics.getFontMetrics();
-        log.debug("Using new/recalculated font with settings: {}", fm);
+		return constructImagePair(bufferedImage);
+	}
 
-        int lineHeight = fm.getHeight();
-        int yInitialOffset = (lines.length - 1) * lineHeight;
+	protected void drawCenteredMessage(Graphics2D graphics, String text) {
+		String message = text.strip();
 
-        int x = 0;
-        int y = (fm.getAscent() + (DISPLAY_HEIGHT - (fm.getAscent() + fm.getDescent()) - yInitialOffset) / 2);
+		FontMetrics initialFm = graphics.getFontMetrics();
+		log.debug("Using initial font with settings: {}", initialFm);
 
-        for (String line : lines) {
-            x = (DISPLAY_WIDTH - fm.stringWidth(line)) / 2;
-            graphics.drawString(line, x, y);
-            y += lineHeight;
-        }
-    }
+		// Calculate max font
+		graphics.setColor(Color.white);
+		Font newFont = new Font(FONT_NAME, Font.PLAIN, INITIAL_FONT_SIZE);
+		graphics.setFont(newFont);
+		FontMetrics newFm = graphics.getFontMetrics();
+		String[] lines = message.split("\n");
+		// int stringWidth = initialFm.stringWidth(text) + 2 * BORDER_WIDTH;
+		int stringWidth = Arrays.stream(lines)
+			.map(line -> newFm.stringWidth(line) + 2 * BORDER_WIDTH)
+			.mapToInt(v -> v)
+			.max()
+			.orElseThrow(NoSuchElementException::new);
+		double widthRatio = (double) DISPLAY_WIDTH / (double) stringWidth;
 
-    protected Pair<String, byte[]> constructImagePair(BufferedImage image) throws IOException {
-        ByteArrayOutputStream output = new ByteArrayOutputStream();
-        ImageIO.write(image, "png", output);
-        String base64Image = Base64.getEncoder().encodeToString(output.toByteArray());
+		int newFontSize = (int) (newFont.getSize() * widthRatio);
+		int fontSizeToUse = Math.min(newFontSize, MAX_EMOJI_FONT_SIZE);
+		Font finalFont = new Font(newFont.getName(), newFont.getStyle(), fontSizeToUse);
+		graphics.setFont(finalFont);
 
-        return new Pair("data:image/png;base64," + base64Image, output.toByteArray());
-    }
+		// Draw centered string
+		FontMetrics fm = graphics.getFontMetrics();
+		log.debug("Using new/recalculated font with settings: {}", fm);
+
+		int lineHeight = fm.getHeight();
+		int yInitialOffset = (lines.length - 1) * lineHeight;
+
+		int x = 0;
+		int y = (fm.getAscent() + (DISPLAY_HEIGHT - (fm.getAscent() + fm.getDescent()) - yInitialOffset) / 2);
+
+		for (String line : lines) {
+			x = (DISPLAY_WIDTH - fm.stringWidth(line)) / 2;
+			graphics.drawString(line, x, y);
+			y += lineHeight;
+		}
+	}
+
+	protected Pair<String, byte[]> constructImagePair(BufferedImage image) throws IOException {
+		ByteArrayOutputStream output = new ByteArrayOutputStream();
+		ImageIO.write(image, "png", output);
+		String base64Image = Base64.getEncoder().encodeToString(output.toByteArray());
+
+		return new Pair("data:image/png;base64," + base64Image, output.toByteArray());
+	}
+
 }
