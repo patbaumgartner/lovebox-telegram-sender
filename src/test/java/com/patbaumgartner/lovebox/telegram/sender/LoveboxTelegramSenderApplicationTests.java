072278cd7794011f8903ce711270e1430ea338ae
Patrick Baumgartner <patrick.baumgartner@42talents.com>
2024-07-03 22:10:21 +0200

    Disabling AppCDS again
    Signed-off-by: Patrick Baumgartner <contact@patbaumgartner.com>

--- src/test/java/com/patbaumgartner/lovebox/telegram/sender/LoveboxTelegramSenderApplicationTests.java
+++ src/test/java/com/patbaumgartner/lovebox/telegram/sender/LoveboxTelegramSenderApplicationTests.java
@@ -2,10 +2,8 @@ package com.patbaumgartner.lovebox.telegram.sender;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
-@TestPropertySource(properties = { "lovebox.enabled=false", "telegrambots.enabled=false" })
+@SpringBootTest(properties = { "lovebox.enabled=false", "telegrambots.enabled=false" })
 class LoveboxTelegramSenderApplicationTests {
 
 	@Test
