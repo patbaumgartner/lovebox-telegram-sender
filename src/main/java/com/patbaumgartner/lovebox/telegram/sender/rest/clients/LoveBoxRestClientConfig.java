46f74dd9d55649a90a381d9becbea95ed2cd68c2
Patrick Baumgartner <contact@patbaumgartner.com>
2025-07-21 13:19:38 +0200

    Formatting

--- src/main/java/com/patbaumgartner/lovebox/telegram/sender/rest/clients/LoveBoxRestClientConfig.java
+++ src/main/java/com/patbaumgartner/lovebox/telegram/sender/rest/clients/LoveBoxRestClientConfig.java
@@ -9,10 +9,11 @@ import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 @Configuration
 public class LoveBoxRestClientConfig {
 
+	private final String LOVEBOX_REST_API = "https://app-api.loveboxlove.com";
+
 	@Bean
 	public LoveboxRestClient loveboxRestClient() {
-		String restApi = "https://app-api.loveboxlove.com";
-		RestClient restClient = RestClient.create(restApi);
+		RestClient restClient = RestClient.create(LOVEBOX_REST_API);
 
 		HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient))
 			.build();
